7bf1c403526f0439e813dfd71f01d5db74e3274b
MagiFeeney <leiferis@outlook.com>
2025-01-24 13:03:48 +0800

    update dependency

--- system-dependency.sh
+++ system-dependency.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# JetBrainsMono font installation
 sudo apt install curl
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+
+# install ripgrep for consult ripgrep
+sudo apt update
+sudo apt install ripgrep
+
